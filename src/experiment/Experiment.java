4528f7b8451e4d910699f191450be07cf539a0c8
Bunathi Buthelezi <Bunathi Buthelezi@192.168.137.103>
2025-04-09 09:52:33 +0200

    First Commit

---
+++ src/experiment/Experiment.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package experiment;
+
+/**
+ *
+ * @author Bunathi Buthelezi
+ */
+public class Experiment {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.print("hello world");
+    }
+    
+}
