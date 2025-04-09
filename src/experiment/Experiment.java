fb7f19c30f7892c1875575f1a3c9c48468dbf9ad
Bunathi Buthelezi <Bunathi Buthelezi@192.168.137.103>
2025-04-09 10:25:12 +0200

    First Commit Second Commit

--- src/experiment/Experiment.java
+++ src/experiment/Experiment.java
@@ -15,6 +15,8 @@ public class Experiment {
      */
     public static void main(String[] args) {
        System.out.print("hello world");
+       System.out.print("Ekse meneer");
+       
     }
     
 }
