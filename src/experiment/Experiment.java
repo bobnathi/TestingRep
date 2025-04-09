c585fbf5f90d0a4846f417bf1b51e17169cfedc7
Bunathi Buthelezi <Bunathi Buthelezi@192.168.137.103>
2025-04-09 10:29:34 +0200

    Fourth Commit

--- src/experiment/Experiment.java
+++ src/experiment/Experiment.java
@@ -17,6 +17,7 @@ public class Experiment {
        System.out.print("hello world");
        System.out.print("Ekse meneer");
        System.out.print("Ekse gazi");
+       System.out.print("Ekse broer");
     }
     
 }
